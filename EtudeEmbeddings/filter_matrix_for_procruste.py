fcb50440f19c09a4cdbb84a508bba5b3f8cc9731
Nicolas Dugué <nicolas.dugue@univ-lemans.fr>
2019-07-01 11:07:57 +0200

    Low filters

--- EtudeEmbeddings/filter_matrix_for_procruste.py
+++ EtudeEmbeddings/filter_matrix_for_procruste.py
@@ -52,3 +52,29 @@ for ligne in fichier:
         fichier_res.write(ligne)
 fichier_res.close()
 fichier.close()
+
+fichier=open("distribution_gdn_sorted.txt")
+dico=set()
+for ligne in fichier:
+    tab=ligne.split(" ")
+    if int(tab[1].strip()) > 100:
+        dico.add(tab[0].strip())
+fichier.close()
+
+fichier=open("frwac_common_sorted.txt")
+fichier_res=open("frwac_common_sorted_Lowfiltered.txt","w")
+for ligne in fichier:
+    tab=ligne.split(" ")
+    if tab[0].strip() in dico:
+        fichier_res.write(ligne)
+fichier_res.close()
+fichier.close()
+
+fichier=open("gdn_common_sorted.txt")
+fichier_res=open("gdn_common_sorted_Lowfiltered.txt","w")
+for ligne in fichier:
+    tab=ligne.split(" ")
+    if tab[0].strip() in dico:
+        fichier_res.write(ligne)
+fichier_res.close()
+fichier.close()

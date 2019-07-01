0bdc684136d0d85d5dd7877fe4f92f54c6dc85ce
Nicolas Dugué <nicolas.dugue@univ-lemans.fr>
2019-07-01 10:46:00 +0200

    Adds filtered matrix

---
+++ EtudeEmbeddings/filter_matrix_for_procruste.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+fichier=open("distribution_gdn_sorted.txt")
+dico=set()
+for ligne in fichier:
+    tab=ligne.split(" ")
+    if int(tab[1].strip()) > 500:
+        dico.add(tab[0].strip())
+fichier.close()
+
+fichier=open("frwac_common_sorted.txt")
+fichier_res=open("frwac_common_sorted_filtered.txt","w")
+for ligne in fichier:
+    tab=ligne.split(" ")
+    if tab[0].strip() in dico:
+        fichier_res.write(ligne)
+fichier_res.close()
+fichier.close()
+
+fichier=open("gdn_common_sorted.txt")
+fichier_res=open("gdn_common_sorted_filtered.txt","w")
+for ligne in fichier:
+    tab=ligne.split(" ")
+    if tab[0].strip() in dico:
+        fichier_res.write(ligne)
+fichier_res.close()
+fichier.close()
